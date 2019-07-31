8a5fd811912597d8af6b2deb83826e9676587976
Stephen Ochieng <stephenojwang1040@gmail.com>
2019-07-31 07:19:13 +0300

    completed !

--- guessing-game.py
+++ guessing-game.py
@@ -4,15 +4,16 @@ secret_word = "antelope"
 guess = ""
 guess_count = 0
 guess_limit = 3
-out_of_guesses = False
+out_of_count = False
 
-while guess != secret_word and not(out_of_guesses):
+while guess != secret_word and not(out_of_count):
     if guess_count < guess_limit:
         guess = input("Enter guess:")
         guess_count += 1
     else:
-        out_of_guesses = True
-print("you failed!")
-
-    
+        out_of_count = True   
+if out_of_count:
+    print("Out of guesses. YOU LOSE!")  
+else:
+    print("YOU WIN!")  
 
